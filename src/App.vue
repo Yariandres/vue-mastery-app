a009518d2d5a4d1a9be994b38a94a0a5f19cc276
Yari Herrera <yari.andres@gmail.com>
2023-11-04 23:13:11 +0100

    adds dynamic data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+import PageHome from './components/PageHome.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <page-home />
 </template>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import sourceData from '../data.json';
+const { threads, posts, users } = sourceData;
+
+function postById(postId: string) {
+  return posts.find((p) => p.id === postId);
+}
+
+function userById(userId: string) {
+  return users.find((u) => u.id === userId);
+}
+</script>
+
+<template>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>
+        {{ userById(postById(postId)?.userId).name }}
+      </p>
+      <p>{{ postById(postId).text }}</p>
+    </div>
+  </div>
+</template>
