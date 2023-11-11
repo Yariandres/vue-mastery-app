9348d56219edcb0f62f67555c828648e12fd2b61
Yari Herrera <yari.andres@gmail.com>
2023-11-11 22:00:59 +0100

    Add PostEditor component and update addPost function

--- src/components/pages/PageThreadShow.vue
+++ src/components/pages/PageThreadShow.vue
@@ -2,6 +2,16 @@
 import { computed, ref } from 'vue';
 import PostList from '../PostList.vue';
 import sourceData from '../../data.json';
+import PostEditor from '../PostEditor.vue';
+
+interface EventData {
+  post: {
+    text: string;
+    publishedAt: number;
+    id: string;
+    userId: string;
+  };
+}
 
 const props = defineProps<{
   id: string;
@@ -9,65 +19,42 @@ const props = defineProps<{
 
 const threads = ref(sourceData.threads);
 const posts = ref(sourceData.posts);
-const newPostText = ref();
 
-const thread = computed(() => {
-  return threads.value.find((thread: any) => thread.id === props.id);
+const thread = computed<any>(() => {
+  return threads.value.find((thread) => thread.id === props.id);
 });
 
-const threadPosts = computed(() => {
+const threadPosts = computed<any>(() => {
   return posts.value.filter((post: any) => post.threadId === props.id);
 });
 
-const addPost = () => {
-  const postId = Math.random().toString(36).substr(2, 9);
+const addPost = (eventData: EventData) => {
   const post = {
-    text: newPostText.value,
-    publishedAt: Math.floor(Date.now() / 1000),
-    id: postId,
-    userId: 'rpbB8C6ifrYmNDufMERWfQUoa202',
+    ...eventData.post,
     threadId: props.id,
   };
 
   posts.value.push(post);
-  thread.value?.posts.push(postId + 'Yoooo ammam');
-  newPostText.value = '';
+  thread.value.posts.push(post.id);
 };
-
-//      @input="newPostText = ($event.target as HTMLTextAreaElement).value"
 </script>
 
 <template>
   <div v-if="thread" class="col-large push-top">
     <h1>{{ thread.title }}</h1>
-
     <post-list :posts="threadPosts" />
 
     <div class="col-full">
-      <form @submit.prevent="addPost">
-        <div class="from-group">
-          <textarea
-            v-model="newPostText"
-            name=""
-            id=""
-            cols="30"
-            rows="10"
-            class="form-input"
-          ></textarea>
-        </div>
-
-        <div class="form-actions">
-          <button class="btn-blue" type="submit">Submit Post</button>
-        </div>
-      </form>
+      <post-editor @update:save="addPost" />
     </div>
   </div>
+
   <div v-else>
     <div class="col-full text-center">
       <h1>This Thread does not exist</h1>
       <router-link :to="{ name: 'Home' }">
-        &#8592; Back to some cool threads</router-link
-      >
+        &#8592; Back to some cool threads
+      </router-link>
     </div>
   </div>
 </template>
