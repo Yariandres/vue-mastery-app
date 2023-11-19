20da2463c0746b61363b198b9a8a6d975854dfe7
Yari Herrera <yari@primeinsights.com>
2023-11-19 23:13:42 +0100

    Refactor ForumList component to include thread count

--- src/components/ForumList.vue
+++ src/components/ForumList.vue
@@ -5,25 +5,27 @@ defineProps<{
 }>();
 </script>
 <template>
-  <div class="forum-list">
-    <h2 class="list-title">
-      <a href="#">Forum</a>
-    </h2>
+  <div class="col-full">
+    <div class="forum-list">
+      <h2 class="list-title">
+        <a href="#">Forum</a>
+      </h2>
 
-    <div class="forum-listing" v-for="forum in forums" :key="forum.id">
-      <div class="forum-details">
-        <router-link
-          :to="{ name: 'Forum', params: { id: forum.id } }"
-          class="text-xlarge"
-          >{{ forum.name }}</router-link
-        >
-        <p>{{ forum.description }}</p>
-      </div>
-      <div class="threads-cout">
-        <p>
-          <span class="count">{{ forum.threads?.length }}</span
-          >{{ forum.threads?.length > 1 ? 'Threads' : 'thread' }}
-        </p>
+      <div class="forum-listing" v-for="forum in forums" :key="forum.id">
+        <div class="forum-details">
+          <router-link
+            :to="{ name: 'Forum', params: { id: forum.id } }"
+            class="text-xlarge"
+            >{{ forum.name }}</router-link
+          >
+          <p>{{ forum.description }}</p>
+        </div>
+        <div class="threads-cout">
+          <p>
+            <span class="count">{{ forum.threads?.length || 0 }}</span
+            >{{ forum.threads?.length === 1 ? ' Thread' : ' Threads' }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
