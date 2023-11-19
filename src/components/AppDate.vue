9a868a5241b242217f78fcb097e694277f237804
Yari Herrera <yari@primeinsights.com>
2023-11-19 21:15:37 +0100

    Add appDate helper functions and update AppDate component

--- src/components/AppDate.vue
+++ src/components/AppDate.vue
@@ -1,25 +1,12 @@
 <script setup lang="ts">
-import daysjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import localizedDate from 'dayjs/plugin/localizedFormat';
+import { diffForHummans, humanFriendlyDate } from '../helpers/appDate';
 
-daysjs.extend(relativeTime);
-daysjs.extend(localizedDate);
-
-const props = defineProps<{
+defineProps<{
   timestamp: number;
 }>();
-
-function diffForHummans() {
-  return daysjs.unix(props.timestamp).fromNow();
-}
-
-function homanFriendlyDate() {
-  return daysjs.unix(props.timestamp).format('MMMM DD, YYYY [at] hh:mm A');
-}
 </script>
 <template>
-  <span :title="homanFriendlyDate()" v-bind="$attrs">
-    {{ diffForHummans() }}
+  <span :title="humanFriendlyDate(timestamp)" v-bind="$attrs">
+    {{ diffForHummans(timestamp) }}
   </span>
 </template>
